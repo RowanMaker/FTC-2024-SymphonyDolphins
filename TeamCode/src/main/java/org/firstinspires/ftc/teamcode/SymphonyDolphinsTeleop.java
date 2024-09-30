31f86f5227db6fd897409573e99ba421fab12b12
Rowan Myers <rwnmyers@gmail.com>
2024-09-30 16:54:03 -0400

    Commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SymphonyDolphinsTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SymphonyDolphinsTeleop.java
@@ -1,5 +1,92 @@
 package org.firstinspires.ftc.teamcode;
+//Defining the code as linear, importing motors, servos, etc
+//defining the code as TeleOp
 
-public class SymphonyDolphinsTeleop {
-    System.out.println("Hello World!");
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+// defining motors
+@TeleOp
+public class SymphonyDolphins extends LinearOpMode {
+    private DcMotor insertMotorNameHere; // port location
+
+    private Servo insertServoNameHere;
+    private CRServo insertLinActNameHere; //servo port location
+
+ // start of OpMode
+    @Override
+    public void runOpMode () {
+        insertMotorNameHere = hardwareMap.dcMotor.get("motorNameDrive");
+
+        insertServoNameHere = hardwareMap.get(Servo.class, "servoName");
+        insertLinActNameHere = hardwareMap.get(CRServo.class, "LinAct");
+
+  // Defining controller methods
+        // left and right sticks (x and y axies)
+        double LX;
+        double LY;
+        double RX;
+        double RY;
+
+        // T triggers
+        double LT;
+        double RT;
+
+        // B bumpers
+        boolean LB;
+        boolean RB;
+
+        // X, Y, A, B buttons
+        boolean X;
+        boolean Y;
+        boolean A;
+        boolean B;
+
+        // D pad
+        boolean DU;
+        boolean DD;
+        boolean DL;
+        boolean DR;
+
+        // bumpers true/false
+        boolean RBhasbeenpressed = false;
+        boolean LBhasbeenpressed = false;
+
+        double speed = 1;
+
+ // configure motors- reverse motors to simplify coding
+    insertMotorNameHere.setDirection(DcMotor.Direction.REVERSE);
+    insertServoNameHere.setDirection(Servo.Direction.REVERSE);
+
+ // Official start of code
+        waitForStart();
+        while(opModeIsActive()) {
+        // Defining controller buttons and values
+            // left and right sticks
+            LX = gamepad1.left_stick_x;
+            LY = gamepad1.left_stick_y * -1;
+            RX = gamepad1.right_stick_x;
+            RY = gamepad1.right_stick_y * -1;
+            // triggers
+            LT = gamepad1.left_trigger;
+            RT = gamepad1.right_trigger;
+            // bumpers
+            LB = gamepad1.left_bumper;
+            RB = gamepad1.right_bumper;
+            // letter buttons
+            X = gamepad1.x;
+            Y = gamepad1.y;
+            A = gamepad1.a;
+            B = gamepad1.b;
+            // D pad
+            DU = gamepad1.dpad_up;
+            DD = gamepad1.dpad_down;
+            DL = gamepad1.dpad_left;
+            DR = gamepad1.dpad_right;
+
+        }
+    }
 }
